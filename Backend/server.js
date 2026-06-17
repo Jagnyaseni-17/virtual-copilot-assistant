d6bc02c354ac2b0ae213f8510c990c665e0c5ad2
JagnyaseniPanda <pandajagnyaseni17@gmail.com>
2026-06-17 22:10:09 +0530

    Virtual AI Assistant

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 9000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRouter);
 
 app.listen(PORT, () => {
   connectDb()
